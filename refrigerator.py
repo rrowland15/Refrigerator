97582b0c19f86f491b02af6cfa8dd18328ead3a0
cslawrence01 <csinsight001@gmail.com>
2022-10-07 13:55:32 -0500

    added sqlite database

--- refrigerator.py
+++ refrigerator.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, render_template
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+sqlconnection = sqlite3.connect('recipes.db')
 
 app = Flask(__name__)
+
+db_name =
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +
+
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         return ''
+
     else:
         #return "This is a get request"
         return render_template('refrigerator2.html')
