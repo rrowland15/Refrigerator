9a5d993a0ece795a15447fae983b8e9737e281e6
ryanrowland <rowlandr@oregonstate.edu>
2022-10-07 15:07:46 -0700

    Fixed gfg function that had space being added when saved

--- refrigerator.py
+++ refrigerator.py
@@ -1,26 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
 import sqlite3
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+#from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy.sql import text
 
 sqlconnection = sqlite3.connect('recipes.db')
 
 app = Flask(__name__)
 
-db_name =
+# db_name =
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +
 
 
 @app.route("/", methods=['GET', 'POST'])
-def home():
+def gfg():
     if request.method == 'POST':
-        return ''
+        first_ingredient = request.form.get("Ingredient1")
+        first_expiration = request.form.get("Ingredient1expiration")
+        return "Your name is "+first_ingredient+first_expiration
+    return render_template("refrigerator2.html")
 
-    else:
-        #return "This is a get request"
-        return render_template('refrigerator2.html')
 
 if __name__ == "__main__":
     app.run()
