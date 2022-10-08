349f3860e4d884e79d56787262be9377200bd6ba
ryanrowland <rowlandr@oregonstate.edu>
2022-10-08 10:55:45 -0700

    installed pip install requests

--- refrigerator.py
+++ refrigerator.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, url_for, redirect
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 import json
+import requests
 
 #from sqlalchemy.sql import text
 
@@ -89,11 +90,12 @@ def myfridge():
 # To navigate to the recipe.html page
 @app.route("/recipe", methods=['GET'])
 def recipe():
-    base_url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients"
+    base_url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="
     postfix_url = "&number=1&ignorePantry=true&apiKey=291bc42edd5b45fca7c83089d1f1da9b"
     temporary = "orange,+banana"
     get_url = base_url + temporary + postfix_url
-    api_response = request.get(get_url)
+    print(get_url)
+    api_response = requests.get(get_url)
     print(api_response)
     if request.method == 'POST':
         pass
